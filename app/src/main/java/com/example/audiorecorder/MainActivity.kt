758af277c6b516a09e97bafbf99bde2de7bc6723
kainan <kainanos@outlook.com>
2024-05-01 11:57:56 +0800

    minor changes

--- app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -20,20 +20,22 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private var isStart = false
-    private var bufferSizeInBytes = 0
+    private var minBufSizeInBytes = 0
+    private var numOfMinBuf = 2
     private var audioRecord: AudioRecord? = null
 
     companion object {
         private const val LOG_TAG = "AudioRecorder"
         // MIC VOICE_UPLINK VOICE_CALL VOICE_RECOGNITION VOICE_COMMUNICATION
         private const val AUDIO_SOURCE = MediaRecorder.AudioSource.MIC
-        private const val SAMPLE_RATE = 16000
+        private const val SAMPLE_RATE = 48000
         private const val CHANNEL_MASK = AudioFormat.CHANNEL_IN_MONO
         private const val ENCODING = AudioFormat.ENCODING_PCM_16BIT
         private const val CHANNELS = 1       // just for dump file name
         private const val PCM_ENCODING = 16  // just for dump file name
         // dump file path: /storage/emulated/0/Android/data/com.example.audiorecorder/files/Music
         private const val DUMP_FILE = "${SAMPLE_RATE}Hz_${CHANNELS}ch_${PCM_ENCODING}bit_record.pcm"
+        // private const val DUMP_FILE1 = "/data/record_48k_1ch_16bit.raw"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,12 +63,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun initAudioCapture() {
         Log.i(LOG_TAG, "initAudioCapture")
-        bufferSizeInBytes = AudioRecord.getMinBufferSize(
+        minBufSizeInBytes = AudioRecord.getMinBufferSize(
             SAMPLE_RATE,
             CHANNEL_MASK,
             ENCODING
         )
-        Log.i(LOG_TAG, "AudioRecord getMinBufferSize: $bufferSizeInBytes")
+        Log.i(LOG_TAG, "AudioRecord getMinBufferSize: $minBufSizeInBytes")
 
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -87,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                     .setEncoding(ENCODING)
                     .build()
             )
-            .setBufferSizeInBytes(bufferSizeInBytes*2)
+            .setBufferSizeInBytes(minBufSizeInBytes * numOfMinBuf)
             .build()
         Log.i(
             LOG_TAG, "set AudioRecord params: " +
@@ -110,7 +112,7 @@ class MainActivity : AppCompatActivity() {
 
                 var fileOutputStream: FileOutputStream? = null
                 try {
-                    val buffer = ByteArray(bufferSizeInBytes*2)
+                    val buffer = ByteArray(minBufSizeInBytes * numOfMinBuf)
                     val currentDate = Calendar.getInstance().time
                     val dateFormat = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
                     val formattedDate = dateFormat.format(currentDate)
@@ -121,7 +123,7 @@ class MainActivity : AppCompatActivity() {
                     audioRecord?.startRecording()
 
                     while (audioRecord!=null) {
-                        val bytesRead = audioRecord?.read(buffer, 0, bufferSizeInBytes*2)!!
+                        val bytesRead = audioRecord?.read(buffer, 0, minBufSizeInBytes * numOfMinBuf)!!
                         if (bytesRead > 0) fileOutputStream.write(buffer,0,bytesRead)
                     }
                 } catch (e: Exception) {
