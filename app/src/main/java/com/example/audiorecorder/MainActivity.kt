cf1305bacaa60325ffc4186d9db6820cb91d4c73
kainan <kainanos@outlook.com>
2024-07-20 11:26:57 +0800

    20240720

--- app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -7,13 +7,15 @@ import android.media.AudioRecord
 import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// import android.os.Environment
 import android.util.Log
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import java.io.File
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
-import java.io.IOException
+// import java.io.IOException
+// import android.os.Environment
+// import java.io.RandomAccessFile
 // import java.text.SimpleDateFormat
 // import java.util.Calendar
 // import java.util.Locale
@@ -23,18 +25,17 @@ class MainActivity : AppCompatActivity() {
     private var numOfMinBuf = 2
     private var minBufSizeInBytes = 0
     private var audioRecord: AudioRecord? = null
+    private var fileOutputStream: FileOutputStream? = null
 
     companion object {
         private const val LOG_TAG = "AudioRecorder"
         // MIC VOICE_UPLINK VOICE_CALL VOICE_RECOGNITION VOICE_COMMUNICATION
         private const val AUDIO_SOURCE = MediaRecorder.AudioSource.MIC
-        private const val SAMPLE_RATE = 48000
-        private const val CHANNEL_MASK = AudioFormat.CHANNEL_IN_MONO
-        private const val ENCODING = AudioFormat.ENCODING_PCM_16BIT
-        // private const val CHANNELS = 1       // just for dump file name
-        // private const val PCM_ENCODING = 16  // just for dump file name
+        private var SAMPLE_RATE = 48000
+        private var CHANNEL_MASK = AudioFormat.CHANNEL_IN_MONO
+        private var ENCODING = AudioFormat.ENCODING_PCM_16BIT
         // private const val DUMP_FILE = "${SAMPLE_RATE}Hz_${CHANNELS}ch_${PCM_ENCODING}bit_record.pcm"
-        private const val DUMP_FILE = "/data/record_48k_1ch_16bit.raw" // need to create the file manually
+        private const val DUMP_FILE = "/data/record_48k_1ch_16bit.wav" // need to create the file manually
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,12 +61,9 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun initAudioCapture() {
+    private fun initAudioCapture(): Boolean {
         Log.i(LOG_TAG, "initAudioCapture")
-        minBufSizeInBytes = AudioRecord.getMinBufferSize(
-            SAMPLE_RATE,
-            CHANNEL_MASK,
-            ENCODING)
+        minBufSizeInBytes = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_MASK, ENCODING)
         Log.i(LOG_TAG, "AudioRecord getMinBufferSize: $minBufSizeInBytes")
 
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
@@ -76,7 +74,7 @@ class MainActivity : AppCompatActivity() {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for Activity#requestPermissions for more details.
-            return
+            return false
         }
         audioRecord = AudioRecord.Builder()
             .setAudioSource(AUDIO_SOURCE)
@@ -96,8 +94,41 @@ class MainActivity : AppCompatActivity() {
                 "Encoding $ENCODING, " +
                 "BufferSizeInFrames ${audioRecord!!.bufferSizeInFrames}")
 
-        // specify the device address with setPreferredDevice
+        // var channelCount = channelCountFromInChannelMask(CHANNEL_MASK)
+        // var bytesPerSample = AudioFormat.getBytesPerSample(ENCODING)
+        val channelCount = when (CHANNEL_MASK) {
+            AudioFormat.CHANNEL_IN_MONO -> 1
+            AudioFormat.CHANNEL_IN_STEREO -> 2
+            else -> 1
+        }
+        val bytesPerSample = when (ENCODING) {
+            AudioFormat.ENCODING_PCM_8BIT -> 1
+            AudioFormat.ENCODING_PCM_16BIT -> 2
+            AudioFormat.ENCODING_PCM_24BIT_PACKED -> 3
+            AudioFormat.ENCODING_PCM_32BIT -> 4
+            else -> 2
+        }
+
+        // val currentDate = Calendar.getInstance().time
+        // val dateFormat = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+        // val formattedDate = dateFormat.format(currentDate)
+        // dump file path: /storage/emulated/0/Android/data/com.example.audiorecorder/files/Music
+        // val outputFile = File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "${formattedDate}_${DUMP_FILE}")
+        val outputFile = File(DUMP_FILE)
+        try {
+            fileOutputStream = FileOutputStream(outputFile)
+            writeWavHeader(fileOutputStream, SAMPLE_RATE, channelCount, bytesPerSample*8)
+        } catch (e: SecurityException) {
+            Log.e(LOG_TAG, "no permission to access the audio file")
+            return false
+        } catch (e: FileNotFoundException) {
+            Log.e(LOG_TAG, "audio file can't be created or opened")
+            return false
+        }
+        Log.i(LOG_TAG, "record audio file: $outputFile")
+
         /*
+        // specify the device address with setPreferredDevice
         val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
         val devices = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS)
         for (device in devices) {
@@ -108,62 +139,58 @@ class MainActivity : AppCompatActivity() {
             }
         }
         */
+        return true
     }
 
     private fun startAudioCapture() {
-        Log.i(LOG_TAG,"start AudioCapture, isStart: $isStart")
-        if (isStart){
+        if (isStart) {
             Log.i(LOG_TAG,"in recording status, needn't start again")
             return
         }
+        Log.i(LOG_TAG,"start AudioCapture")
+        isStart = true
+        if (!initAudioCapture()) {
+            isStart = false
+            return
+        }
+        startCapture()
+    }
+
+    private fun stopAudioCapture() {
+        if (!isStart) {
+            Log.i(LOG_TAG,"in stop status, needn't stop again")
+            return
+        }
+        Log.i(LOG_TAG,"stop AudioCapture")
+        isStart = false
+    }
+
+    private fun startCapture() {
         class AudioRecordThread: Thread() {
             override fun run() {
                 super.run()
 
-                var fileOutputStream: FileOutputStream? = null
-                try {
-                    val buffer = ByteArray(minBufSizeInBytes)
-                    // val currentDate = Calendar.getInstance().time
-                    // val dateFormat = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
-                    // val formattedDate = dateFormat.format(currentDate)
-                    // dump file path: /storage/emulated/0/Android/data/com.example.audiorecorder/files/Music
-                    // val outputFile = File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "${formattedDate}_${DUMP_FILE}")
-                    val outputFile = File(DUMP_FILE)
-                    fileOutputStream = FileOutputStream(outputFile)
-                    Log.i(LOG_TAG, "record file: $outputFile")
-
-                    audioRecord?.startRecording()
-
-                    while (audioRecord != null) {
-                        if (isStart) {
-                            val bytesRead = audioRecord?.read(buffer, 0, minBufSizeInBytes)!!
-                            if (bytesRead > 0) fileOutputStream.write(buffer,0,bytesRead)
-                        } else {
-                            stopCapture()
+                val buffer = ByteArray(minBufSizeInBytes)
+                if (audioRecord!!.state != AudioRecord.RECORDSTATE_RECORDING) {
+                    audioRecord!!.startRecording()
+                    sleep(5)
+                }
+                var totalBytesRead = 0
+                while (audioRecord != null) {
+                    if (isStart) {
+                        val bytesRead = audioRecord?.read(buffer, 0, minBufSizeInBytes)!!
+                        if (bytesRead > 0) {
+                            fileOutputStream?.write(buffer,0,bytesRead)
+                            totalBytesRead += bytesRead
                         }
-                    }
-                } catch (e: Exception) {
-                    Log.e(LOG_TAG, "Exception: ")
-                    e.printStackTrace()
-                } finally {
-                    try {
-                        fileOutputStream?.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
+                    } else {
+                        stopCapture()
+                        fileOutputStream?.let { updateWavHeader(it,totalBytesRead + 44) }
                     }
                 }
             }
         }
-        initAudioCapture()
-        isStart = true
         AudioRecordThread().start()
-    }
-
-    private fun stopAudioCapture() {
-        Log.i(LOG_TAG,"stop AudioCapture, isStart: $isStart")
-        if (isStart) {
-            isStart = false
-        }
     }
 
     private fun stopCapture() {
@@ -171,4 +198,85 @@ class MainActivity : AppCompatActivity() {
         audioRecord?.release()
         audioRecord = null
     }
+
+
+    /*************** wav header function **********************************/
+    private fun writeWavHeader(file: FileOutputStream?, sampleRate: Int, channels: Int, bitsPerSample: Int) {
+        val header = ByteArray(44)
+        val byteRate = sampleRate * channels * bitsPerSample / 8
+
+        // RIFF header
+        header[0] = 'R'.code.toByte()
+        header[1] = 'I'.code.toByte()
+        header[2] = 'F'.code.toByte()
+        header[3] = 'F'.code.toByte()
+        header[8] = 'W'.code.toByte()
+        header[9] = 'A'.code.toByte()
+        header[10] = 'V'.code.toByte()
+        header[11] = 'E'.code.toByte()
+
+        // fmt subchunk
+        header[12] = 'f'.code.toByte()
+        header[13] = 'm'.code.toByte()
+        header[14] = 't'.code.toByte()
+        header[15] = ' '.code.toByte()
+        header[16] = 16 // Subchunk1Size for PCM
+        header[20] = 1 // AudioFormat (1 for PCM)
+        header[22] = channels.toByte()
+        header[24] = (sampleRate and 0xff).toByte()
+        header[25] = ((sampleRate shr 8) and 0xff).toByte()
+        header[26] = ((sampleRate shr 16) and 0xff).toByte()
+        header[27] = ((sampleRate shr 24) and 0xff).toByte()
+        header[28] = (byteRate and 0xff).toByte()
+        header[29] = ((byteRate shr 8) and 0xff).toByte()
+        header[30] = ((byteRate shr 16) and 0xff).toByte()
+        header[31] = ((byteRate shr 24) and 0xff).toByte()
+        header[32] = (channels * bitsPerSample / 8).toByte() // BlockAlign
+        header[34] = bitsPerSample.toByte()
+
+        // data subchunk
+        header[36] = 'd'.code.toByte()
+        header[37] = 'a'.code.toByte()
+        header[38] = 't'.code.toByte()
+        header[39] = 'a'.code.toByte()
+
+        //val outputStream = FileOutputStream(file)
+        file?.write(header)
+        //outputStream.close()
+    }
+
+    fun updateWavHeader(outputStream: FileOutputStream, fileSize: Int) {
+        val dataSize = fileSize - 44
+
+        val headerSize = intToByteArray(fileSize - 8)
+        val dataSizeArray = intToByteArray(dataSize)
+
+        outputStream.channel.position(4)
+        outputStream.write(headerSize)
+
+        outputStream.channel.position(40)
+        outputStream.write(dataSizeArray)
+    }
+
+    private fun intToByteArray(value: Int): ByteArray {
+        return byteArrayOf(
+            (value and 0xff).toByte(),
+            ((value shr 8) and 0xff).toByte(),
+            ((value shr 16) and 0xff).toByte(),
+            ((value shr 24) and 0xff).toByte()
+        )
+    }
+
+//    fun updateWavHeader(file: File) {
+//        val fileSize = file.length().toInt()
+//        val dataSize = fileSize - 44
+//
+//        val raf = RandomAccessFile(file, "rw")
+//        raf.seek(4)
+//        raf.write(intToByteArray(fileSize - 8))
+//
+//        raf.seek(40)
+//        raf.write(intToByteArray(dataSize))
+//        raf.close()
+//    }
 }
